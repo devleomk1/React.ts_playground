8cac9ca19f44ab570b048a3be7c1567d95f47bf0
Kang Jisoon <jisokang@student.42seoul.kr>
2023-01-21 19:22:33 +0900

    feat: 📚 Study Recoil

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, ReactEventHandler } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { atom, useRecoilState } from 'recoil';
+
 
 function App() {
+  const textState = atom({
+    key: 'textState', // unique ID (with respect to other atoms/selectors)
+    default: '디폴트', // default value (aka initial value)
+  });
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (event:React.ChangeEvent<HTMLInputElement>) => { //event 타입 지정 : onChange에 마우스를 올리고 Handler만 지우면됨
+    setText(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> 아주그냥 and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <div>my text : {text}</div>
     </div>
   );
 }
